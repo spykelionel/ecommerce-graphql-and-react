7124a1b7520a78800314b8e5c4d381f582e6a164
spykelionel <social.lionel@gmail.com>
2023-12-04 04:06:58 +0100

    add total cart items to app bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import ProductList from "./components/ProductList";
 import { AppBar, Toolbar, Typography, Container } from "@mui/material";
 
+import { cartSelector } from "./features/product/productSlice";
+import { useSelector } from "react-redux";
+
 function App() {
+  const cart = useSelector(cartSelector);
   return (
     <div>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar style={{ display: "flex", justifyContent: "space-evenly" }}>
           <Typography variant="h6">Product Catalog</Typography>
+          <Typography variant="h6">Cart {cart.length}</Typography>
         </Toolbar>
       </AppBar>
       <Container style={{ padding: "20px" }}>
