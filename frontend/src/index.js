b29587b4aaef6dc14fa77cba1dba6570d2fdd04c
spykelionel <social.lionel@gmail.com>
2023-12-04 07:04:37 +0100

    refactor index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import "./index.css";
 import App from "./App";
-
-import { ApolloProvider } from "@apollo/client";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql", // Adjust the URL accordingly
-  cache: new InMemoryCache(),
-});
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ApolloProvider client={client}>
-        <App />
-      </ApolloProvider>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
